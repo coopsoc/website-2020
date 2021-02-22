5d4eaf2993806c3f8319a437a388b2f9b09bd824
Noa Challis <noa@challis.com.au>
2021-02-22 21:40:43 +1100

    timeline

--- gulpfile.js
+++ gulpfile.js
@@ -133,3 +133,16 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.description-flex-container').find('p');
+	inputs.click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = paras.eq(ind);
+		
+		t.add(matchedPara).addClass('active');
+		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+	});
+});
