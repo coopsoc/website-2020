7e99327c34092db03037a53e3047c923851a7188
noachallis <68141421+noachallis@users.noreply.github.com>
2021-02-22 21:55:23 +1100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -133,16 +133,3 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
-
-$(function(){
-	var inputs = $('.input');
-	var paras = $('.description-flex-container').find('p');
-	inputs.click(function(){
-		var t = $(this),
-				ind = t.index(),
-				matchedPara = paras.eq(ind);
-		
-		t.add(matchedPara).addClass('active');
-		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
-	});
-});
